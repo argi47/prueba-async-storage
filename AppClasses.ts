e50baebb6a435b6e2a6f2810d0bf5ebaa94ed7bb
argi47 <argi47@msn.com>
2024-03-14 16:30:59 +0100

    first-commit

---
+++ AppClasses.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native'
+
+export const classes = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    borderColor: '#666',
+    borderBottomWidth: 1,
+    width: 300,
+    height: 40,
+    marginBottom: 20
+  },
+  btnEliminar: {
+    backgroundColor: 'red',
+    marginTop: 20,
+    padding: 10
+  },
+  textoEliminar: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    width: 300
+  }
+})
